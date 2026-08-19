b0a264ff6dab4d2ca0e6460ea81cb184b0f3e7fd
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2026-08-19 22:01:38 +0200

    chore(security): update script to generate PKCS12 keystores and track new certs

--- security/generate-certs.sh
+++ security/generate-certs.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
-
 cd /work
-
 openssl req -new -x509 -keyout ca.key -out ca.crt -days 365 -passout pass:sentinel -subj "/C=IT/O=KafkaSentinel/CN=SentinelCA"
+
+openssl pkcs12 -export -in ca.crt -nokeys -out truststore.p12 -passout pass:sentinel
+echo "sentinel" > truststore.password
 
 for i in broker-1 broker-2 broker-3 client; do
   mkdir -p ${i}-creds
-  
-  # Generazione della chiave privata
   openssl genrsa -out ${i}-creds/${i}.key 2048
-  
-  # Generazione della richiesta di firma (CSR)
   openssl req -new -key ${i}-creds/${i}.key -out ${i}-creds/${i}.csr -subj "/C=IT/O=KafkaSentinel/CN=${i}"
-  
-  # firma del certificato
   openssl x509 -req -in ${i}-creds/${i}.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out ${i}-creds/${i}.crt -days 365 -passin pass:sentinel
   
+  openssl pkcs12 -export -in ${i}-creds/${i}.crt -inkey ${i}-creds/${i}.key -out ${i}-creds/${i}.keystore.p12 -name ${i} -passout pass:sentinel
+  
+  echo "sentinel" > ${i}-creds/${i}.keystore.password
+  
+  cp truststore.p12 ${i}-creds/
+  cp truststore.password ${i}-creds/
 done
 
-# Pulizia dei file temporanei
-rm *.csr 2>/dev/null || true
-rm *.srl 2>/dev/null || true
-
-echo "Certificati TLS pronti"
+rm *.csr *.srl 2>/dev/null || true
+echo "Certificati pronti."
